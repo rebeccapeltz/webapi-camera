083bb760f778cd27edea53aaca6c87d588fa31f6
rebeccapeltz <rebeccapeltz@gmail.com>
2023-09-24 17:05:15 -0700

    enable a device change to see back of camera on mobile'

--- script.js
+++ script.js
@@ -26,6 +26,18 @@ function disableBtn(id) {
   }
 }
 
+function deviceChange(){
+  stop();
+  //don't change selected device
+  setConstraints();
+  this.getMedia().then(result => {
+    this.isStartEnabled = false;
+    this.cameraState = true;
+    // eslint-disable-next-line no-console
+    console.log("device change:", result);
+  });
+}
+
 function start() {
   stop();
   document.querySelector("#video-container").classList.remove("hidden-video");
@@ -57,6 +69,7 @@ function setConstraints() {
   if (data.selectedDevice === null) {
     videoContstraints.facingMode = "environment";
   } else {
+
     videoContstraints.deviceId = {
       exact: data.selectedDevice,
     };
@@ -77,6 +90,13 @@ async function getMedia() {
   } catch (err) {
     throw err;
   }
+}
+function deviceOptionChange()
+{
+  debugger
+  const value = document.querySelector("#device-option").value;
+  data.selectedDevice = value
+  deviceChange()
 }
 async function getDevices() {
   // trigger prompt for permission
